9424edf5ccf3b3cc62455ea5f9eb7a41a13db41d
burhanudinstei-web <burhanudin.stei@gmail.com>
2026-05-26 12:48:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/react-refresh'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Akademik-MDTU/', //https://burhanudinstei-web.github.io/Akademik-MDTU/
+  base: '/Akademik-MDTU/',
 })
