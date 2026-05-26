57b0f751a2fe424e25feaef163bbfb9f3eb49e31
burhanudinstei-web <burhanudin.stei@gmail.com>
2026-05-26 13:01:11 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh'
+import react from '@vitejs/plugin-react' // Perbaikan di baris ini
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Akademik-MDTU/',
+  base: '/Akademik-MDTU/', 
 })
